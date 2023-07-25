58107cf9e70aae46d705b966af0c1c743a337a46
thuangb <leejohnthuan@gmail.com>
2023-07-26 00:18:28 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 
 $(document).ready(function() {
-    $('.publication-mousecell').mouseover(function() {
+    $('.publication-mousecell').onmouseover(function() {
         $(this).find('video').css('display', 'inline-block');
         $(this).find('img').css('display', 'none');
     });
-    $('.publication-mousecell').mouseout(function() {
+    $('.publication-mousecell').onmouseout(function() {
         $(this).find('video').css('display', 'none');
         $(this).find('img').css('display', 'inline-block');
     });
